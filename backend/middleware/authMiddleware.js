3527d3268ccaae4a882f3101bcfb308411e5653f
Dravingupta <dravingupta12@gmail.com>
2026-01-13 18:03:42 +0530

    middleware added

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -9,12 +9,7 @@ const protect = async (req, res, next) => {
             token = req.headers.authorization.split(' ')[1];
             const decoded = jwt.verify(token, JWT_SECRET);
 
-            // Attach user to request (excluding password)
-            // If we want to be safe, we should fetch from DB to ensure user still exists/has role
-            // For now, using decoded payload is faster but less secure if roles change
-            // Let's try to fetch user if possible, or just use decoded
-            // The previous code used decoded.role directly. 
-            // Let's stick to decoded for now but remove the admin check.
+
             req.user = decoded;
 
             next();
